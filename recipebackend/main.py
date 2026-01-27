98d95d4c77a16c7fded26fa32bda54a71453dd45
Gaji Yaqub <gajiyakub6@gmail.com>
2026-01-27 02:41:50 +0100

    Bug Fixed

--- recipebackend/main.py
+++ recipebackend/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins = "https://recipe-vault-pearl.vercel.app/",
+  allow_origins = "https://recipe-vault-pearl.vercel.app",
   allow_headers = ["*"],
   allow_credentials = True,
   allow_methods = ["*"]
