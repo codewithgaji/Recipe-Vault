6d6549546c79a1f756a51da700ac6ad2bd4539bb
Gaji Yaqub <gajiyakub6@gmail.com>
2026-01-27 02:43:08 +0100

    Done

--- recipefrontend/src/services/api.ts
+++ recipefrontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import { Recipe, RecipeCreate, RecipeUpdate } from "@/types/recipe";
 
-const API_BASE_URL = "recipe-vault-production-43e6.up.railway.app";
+const API_BASE_URL = "https://recipe-vault-production-43e6.up.railway.app";
 
 export class ApiError extends Error {
   status: number;
