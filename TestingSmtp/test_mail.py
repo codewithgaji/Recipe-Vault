cdeaaef3854bbe0a2d4e28d7fedd6f94a6ddcb38
Gaji Yaqub <gajiyakub6@gmail.com>
2026-02-04 23:52:52 +0100

    Mailing System has started working

--- TestingSmtp/test_mail.py
+++ TestingSmtp/test_mail.py
@@ -3,32 +3,31 @@ import smtplib
 from email.message import EmailMessage
 from dotenv import load_dotenv
 
+
 load_dotenv(r"C:\Users\Gaji\Documents\Recipe Vault\recipebackend\.env")
 
 GMAIL_EMAIL = os.getenv("GMAIL_EMAIL")
 GMAIL_APP_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
 
+
+
 if not GMAIL_EMAIL or not GMAIL_APP_PASSWORD:
-  raise ValueError("Missing GMAIL_EMAIL or GMAIL_APP_PASSWORD in .env")
+  raise ValueError("GMAIL_EMAIL or GMAIL_APP_PASSWORD not found in .env")
 
 GMAIL_APP_PASSWORD = GMAIL_APP_PASSWORD.replace(" ", "")
 
+msg = EmailMessage()
 
-msg = EmailMessage() # Creating an Object of the EmailMessage() class
-
-msg["Subject"] = "Mail Testing"
+msg["Subject"] = "Testing For Testing Sake, lol"
 msg["From"] = GMAIL_EMAIL
 msg["To"] = "gajiyakub2@gmail.com"
-msg.set_content(
-  "Hi Gaji, \n\n This is a Gmail SMTP test from Python \n\nThanks, \nTest Script"
-)
+msg.set_content("Hello Yk, \n\nI really Love seeing my Python Code being able to send a mail, Lol \n\nLet's Improve Okay, \nThanks")
 
-
-with smtplib.SMTP("smtp.gmail.com", 587) as smtp: # This is to open the gmail email at port 587 as smtp
-  smtp.ehlo() # This Introduces our Program to Gmail's Server "Hello Server"
-  smtp.starttls() # This Turns the connection into an encrypted (secure) connection
-  smtp.ehlo() # We do this again because the server wants us to reintroduce ourself on the SECURE CHANNEL
-  smtp.login(GMAIL_EMAIL, GMAIL_APP_PASSWORD) # Then we login
+with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.ehlo()
+  smtp.login(GMAIL_EMAIL, GMAIL_APP_PASSWORD)
   smtp.send_message(msg)
 
-print("✅ Sent successfully")
+print("SENT SUCCESSFULLY")
